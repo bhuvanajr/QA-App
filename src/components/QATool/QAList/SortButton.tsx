d8b2e6261614163d9d924d0e74c273bebc205c59
Bhuvana Jagadisha <bja@netcompany.com>
2022-04-16 12:20:15 +0200

    Separate spinner , reset button, sort button

---
+++ src/components/QATool/QAList/SortButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useSortContext } from '../../../context';
+
+export const SortButton: React.FC = () => {
+  const {
+    methods: { requestSort, getSortIconClassName },
+  } = useSortContext();
+  const iconClass = getSortIconClassName('question');
+
+  return (
+    <button type="button" className="btn-icon" onClick={() => requestSort('question')}>
+      {iconClass && <div className={`icon ${iconClass}`} />}
+    </button>
+  );
+};
--- src/components/_shared/index.ts
+++ src/components/_shared/index.ts
@@ -1,3 +1,4 @@
 export * from './TextInput';
 export * from './ErrorMessageDisplay';
 export * from './AccordionRow';
+export * from './Spinner';
